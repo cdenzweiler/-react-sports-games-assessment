f19ea32baacc959bda45207b29709de8ac6aa87d
Chris Enzweiler <cdenzweiler@gmail.com>
2021-01-26 15:51:55 -0500

    React Sports Game Assessment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+// import image from './assets/images/square.png';
+import ReactDOM from 'react-dom';
+import Game from './components/game/Game';
+import './index.css';
 
-function App() {
+function App(props) {
+  const raccoons = {
+    name: 'Russiaville Raccoons',
+    logoSrc: "/src/assets/images/raccoon.png"
+  }
+
+  const squirrels = {
+    name: 'Sheridan Squirrels',
+    logoSrc: <img src={"./assets/images/squirrel.png"} alt="" />
+  }
+
+  const bunnies = {
+    name: 'Burlington Bunnies',
+    logoSrc: './assets/images/bunny.png'
+  }
+
+  const hounds = {
+    name: 'Hammond Hounds',
+    logoSrc: './assets/images/hound.png'
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game
+        venue="Union 525 Gem"
+        homeTeam={squirrels}
+        visitingTeam={raccoons}
+      />
+      <Game
+        venue="Sheridan Arena"
+        homeTeam={bunnies}
+        visitingTeam={hounds}
+      />
     </div>
-  );
+  )
 }
 
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     
+    //     <p>
+    //       ooooOAAAHH  FIX IT ALL FIX IT ALL! <code>src/App.js</code> and save the WORLD.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React  ...or else
+    //     </a>
+    //   </header>
+    // </div>
+  
+
+
 export default App;
+
+
+// import { Game } from "Game";
+
+
+// export default Game
